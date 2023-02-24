679fe60400a1899a4dbb4e5c78fae4cc854648d4
Alexander Dreweke <alexander@dreweke.net>
2023-02-24 21:14:39 +0100

    expand schema to take additional fields

--- loxone.sql
+++ loxone.sql
@@ -1,11 +1,13 @@
 CREATE TABLE IF NOT EXISTS room (
-	time		TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-    id          VARCHAR   NOT NULL,
-    name        VARCHAR   NOT NULL,
-    temperature REAL,
-    humidity    REAL,
-    shading     REAL,
-    valve       REAL,
-    ventilation BOOLEAN,
-	UNIQUE		(time, id)
+	time		       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    id                 VARCHAR   NOT NULL,
+    name               VARCHAR   NOT NULL,
+    temperature        REAL,
+    temperature_target REAL,
+    humidity           REAL,
+    light              BOOLEAN,
+    shading            REAL,
+    valve              REAL,
+    ventilation        BOOLEAN,
+	UNIQUE		       (time, id)
 );
