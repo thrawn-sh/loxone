d28c22cc3b64a504bfc317dc408ae397e67e475c
Alexander Dreweke <alexander@dreweke.net>
2023-07-13 13:20:14 +0200

    cleanup tabs to spaces

--- loxone.sql
+++ loxone.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS room (
-	time		       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    time               TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     id                 VARCHAR   NOT NULL,
     name               VARCHAR   NOT NULL,
     temperature        REAL,
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS room (
     shading            REAL,
     valve              REAL,
     ventilation        BOOLEAN,
-	UNIQUE		       (time, id)
+    UNIQUE             (time, id)
 );
