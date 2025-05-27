8c8fa4dc7fe54bd406c83ef830782ee29587a561
Alexander Dreweke <alexander@dreweke.net>
2025-05-27 09:26:48 +0200

    fix typos

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,6 @@ exec python -m loxone.monitor                                                   
     --server="${LOXONE_SERVER}"                                                              \
     --user="${LOXONE_USER}"                                                                  \
     --password="${LOXONE_PASSWORD}"                                                          \
-    --db-uri="postgres://${DATABASE_USER}:${DATABASE_PASSOWRD}@${DATABASE_HOST}/${DATABASE}" \
-    --log-level="${LOG_LEVEL:INFO}"
+    --db-uri="postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE}" \
+    --log-level="${LOG_LEVEL:-INFO}"
 
